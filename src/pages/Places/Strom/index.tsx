3aa7818b2753bf7838fd8f7b53cc57f1339e6b96
c.morais <c.morais1507@gmail.com>
2023-01-18 02:28:43 -0300

    feat: Update Strom Page

--- src/pages/Places/Strom/index.tsx
+++ src/pages/Places/Strom/index.tsx
@@ -14,10 +14,38 @@ const StromPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className={`${inter.className} bg-slate-900 text-white scroll-smooth`}>
-        <div className="bg-[url('/images/Strom_tn.png')] h-[500px] w-full bg-no-repeat bg-cover bg-center backdrop-brightness-50 backdrop-blur-sm flex justify-center items-center">
+        <div className="bg-[url('/images/Strom_tn.png')] h-[500px] w-full bg-no-repeat bg-cover bg-center backdrop-brightness-50 backdrop-blur-lg flex items-end">
           <div>
-            <h1 className='text-[68px]'>Glória à cidade de pedra</h1>
+            <h1 className='text-[32px] lg:text-[68px] w-full bg-cyan-900 font-bold p-4 shadow-lg'>Glória à cidade de pedra</h1>
           </div>  
+        </div>
+        <div>
+          <h1 className='text-[32px] lg:text-[56px] text-center font-bold pt-6'>História</h1>
+          <p className='mx-12 py-6 text-justify'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus faucibus tortor, vulputate varius massa mattis eu. In laoreet semper ante, a condimentum est varius ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ex vel neque auctor malesuada. Pellentesque vel dignissim neque. Vivamus efficitur mattis luctus. Suspendisse convallis ipsum nibh, ac auctor arcu eleifend vel. Curabitur vel suscipit risus. Proin vel dapibus quam.
+
+            Phasellus vestibulum, nisi non pretium tempor, urna ex placerat turpis, rutrum scelerisque tortor dolor finibus elit. Fusce non urna id augue efficitur ullamcorper non non diam. Aliquam elementum ullamcorper bibendum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec mattis, sem vitae cursus pharetra, magna leo varius sem, elementum maximus ante augue sed augue. In non lacus ipsum. Aliquam nec tincidunt odio. Aliquam imperdiet justo id nulla pulvinar, in molestie lacus tempor.
+
+            Aliquam ut finibus ante, aliquam vehicula metus. Phasellus mattis accumsan purus, eu blandit sem sollicitudin sit amet. Maecenas ac volutpat ligula. Vivamus sagittis diam vitae nunc sagittis maximus. Sed aliquam, ante ac lobortis ultricies, augue justo sollicitudin nibh, ullamcorper ullamcorper dui leo eget purus. Curabitur viverra, nisl id dapibus iaculis, risus ex fermentum nibh, eu ullamcorper elit massa vitae lorem. Mauris nec orci nibh. Etiam at neque magna. Morbi convallis consectetur tellus eu lacinia. In eleifend faucibus quam nec condimentum. Mauris volutpat metus a varius euismod. Pellentesque at erat non enim interdum cursus in non lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla cursus egestas nunc, ac viverra augue aliquet at.
+
+            Fusce laoreet in urna sed dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fringilla iaculis elit, eu mattis dolor maximus sed. Pellentesque sed congue enim, nec congue nunc. Etiam mattis purus mauris. Donec id egestas nisi. Cras at tellus sed nibh rhoncus laoreet a vitae massa. Morbi venenatis condimentum massa maximus mollis. Donec sed placerat justo. Donec eu iaculis enim. Maecenas suscipit lectus facilisis lacinia iaculis.
+
+            Praesent non justo sed elit commodo consequat. Nunc cursus mi nec risus volutpat fringilla. Morbi auctor sed dolor non iaculis. Maecenas maximus at ante id hendrerit. In scelerisque faucibus pharetra. Praesent in gravida purus, a volutpat urna. Duis id dapibus diam, a convallis ex. Nunc scelerisque diam nec condimentum ornare.
+          </p>
+        </div>
+        <div>
+          <h1 className='text-[32px] lg:text-[56px] text-center font-bold pt-6'>Religião</h1>
+          <p className='mx-12 py-6 text-justify'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus faucibus tortor, vulputate varius massa mattis eu. In laoreet semper ante, a condimentum est varius ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ex vel neque auctor malesuada. Pellentesque vel dignissim neque. Vivamus efficitur mattis luctus. Suspendisse convallis ipsum nibh, ac auctor arcu eleifend vel. Curabitur vel suscipit risus. Proin vel dapibus quam.
+
+            Phasellus vestibulum, nisi non pretium tempor, urna ex placerat turpis, rutrum scelerisque tortor dolor finibus elit. Fusce non urna id augue efficitur ullamcorper non non diam. Aliquam elementum ullamcorper bibendum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec mattis, sem vitae cursus pharetra, magna leo varius sem, elementum maximus ante augue sed augue. In non lacus ipsum. Aliquam nec tincidunt odio. Aliquam imperdiet justo id nulla pulvinar, in molestie lacus tempor.
+
+            Aliquam ut finibus ante, aliquam vehicula metus. Phasellus mattis accumsan purus, eu blandit sem sollicitudin sit amet. Maecenas ac volutpat ligula. Vivamus sagittis diam vitae nunc sagittis maximus. Sed aliquam, ante ac lobortis ultricies, augue justo sollicitudin nibh, ullamcorper ullamcorper dui leo eget purus. Curabitur viverra, nisl id dapibus iaculis, risus ex fermentum nibh, eu ullamcorper elit massa vitae lorem. Mauris nec orci nibh. Etiam at neque magna. Morbi convallis consectetur tellus eu lacinia. In eleifend faucibus quam nec condimentum. Mauris volutpat metus a varius euismod. Pellentesque at erat non enim interdum cursus in non lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla cursus egestas nunc, ac viverra augue aliquet at.
+
+            Fusce laoreet in urna sed dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fringilla iaculis elit, eu mattis dolor maximus sed. Pellentesque sed congue enim, nec congue nunc. Etiam mattis purus mauris. Donec id egestas nisi. Cras at tellus sed nibh rhoncus laoreet a vitae massa. Morbi venenatis condimentum massa maximus mollis. Donec sed placerat justo. Donec eu iaculis enim. Maecenas suscipit lectus facilisis lacinia iaculis.
+
+            Praesent non justo sed elit commodo consequat. Nunc cursus mi nec risus volutpat fringilla. Morbi auctor sed dolor non iaculis. Maecenas maximus at ante id hendrerit. In scelerisque faucibus pharetra. Praesent in gravida purus, a volutpat urna. Duis id dapibus diam, a convallis ex. Nunc scelerisque diam nec condimentum ornare.
+          </p>
         </div>
       </main>
     </>
